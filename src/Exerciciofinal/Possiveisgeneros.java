c611ebe215a966df231afdf3695046c652473dba
Ana Paula Sena <113528275+paulinhasena@users.noreply.github.com>
2025-08-11 19:13:08 -0300

    Update Possiveisgeneros.java

--- src/Exerciciofinal/Possiveisgeneros.java
+++ src/Exerciciofinal/Possiveisgeneros.java
@@ -1,4 +1,13 @@
 package Exerciciofinal;
 
 public enum Possiveisgeneros {
+        documentario,
+        comedia,
+        suspense,
+        animacao,
+        romance,
+        ficcao,
+        acao,
+        drama,
+        terror;
 }
